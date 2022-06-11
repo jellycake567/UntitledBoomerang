2763c99d199bb8c53f3ddac505474bd23d720e70
Samuel <robbo_rules1@live.com.au>
2022-06-11 15:47:51 +1000

    AI Patrol on ground

---
+++ UntitledFoxSpirit/Assets/Scripts/Enemy/AIPatrol.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIPatrol : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    bool mustPatrol = true;
+    bool mustTurn;
+    
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (mustPatrol)
+        {
+            Collider[] overlap = Physics.OverlapSphere(groundCheck.position, 0.1f, groundLayer);
+
+            if (overlap.Length == 0)
+                mustTurn = true;
+            else
+                mustTurn = false;
+        }
+    }
+
+    void Patrol()
+    {
+        if (mustTurn)
+        {
+            Flip();
+        }
+
+        // Move AI
+        Vector3 velocity = new Vector3(speed * Time.fixedDeltaTime, rb.velocity.y, rb.velocity.z);
+        rb.velocity = transform.forward * velocity.magnitude;
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+
+        Vector3 rotation = transform.eulerAngles + Quaternion.Euler(new Vector3(0, 180f, 0)).eulerAngles;
+        transform.rotation = Quaternion.Euler(rotation);
+        speed *= -1;
+
+        mustPatrol = true;
+    }
+}
