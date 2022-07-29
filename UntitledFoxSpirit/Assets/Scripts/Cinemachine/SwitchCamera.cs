5f2f3dbf959f2be37c573b3f2d6c8f79c76b3be3
Samuel <robbo_rules1@live.com.au>
2022-07-29 15:07:35 +1000

    Camera Switching

---
+++ UntitledFoxSpirit/Assets/Scripts/Cinemachine/SwitchCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public CinemachineVirtualCamera activateCam;
+    public CinemachineVirtualCamera deactivateCam;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            activateCam.Priority = 10;
+            deactivateCam.Priority = 0;
+        }
+    }
+}
