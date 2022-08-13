f9f03ac6d005645fc0c097d4e8b2ce5b572c4079
Samuel <robbo_rules1@live.com.au>
2022-08-13 13:36:53 +1000

    weapon detection fixed debug

--- UntitledFoxSpirit/Assets/Scripts/Player/WeaponAttack.cs
+++ UntitledFoxSpirit/Assets/Scripts/Player/WeaponAttack.cs
@@ -22,9 +22,8 @@ public class WeaponAttack : MonoBehaviour
     // ## A list of hardPoints will be referred to a singular hardPoint ##
 
     // Show Debug Lines 
-    List<List<Vector3>> debugHardPoints = new List<List<Vector3>>(); // Stores all the hardPoints
+    List<List<(Vector3, bool)>> debugHardPoints = new List<List<(Vector3, bool)>>(); // Stores all the hardPoints
     List<Vector3> preHardPoints = new List<Vector3>(); // Stores the hardPoint position after raycasting
-    List<Vector3> hardPointHit = new List<Vector3>(); // Store the hardPoint when weapon collides with enemy
 
     #endregion
 
@@ -47,37 +46,23 @@ public class WeaponAttack : MonoBehaviour
         // If there is at least 2 hardPoints
         if (debugHardPoints.Count > 1 && showDebugLines)
         {
-            // Loop through the hardPoints
+            // Loop through the list of hardPoints
             for (int i = 1; i < debugHardPoints.Count; i++)
             {
-                // If hardPoint has hit enemy
-                if (hardPointHit == debugHardPoints[i])
-                    Gizmos.color = Color.red;
-                else
-                    Gizmos.color = Color.blue;
-
-                // Draw the lines between hardPoints
+                // Loop through hardpoints in the list
                 for (int j = 0; j < debugHardPoints[i].Count; j++)
                 {
-                    Gizmos.color = Color.blue;
-
-                    // Distance between current weapon position and last weapon position
-                    float distance = Vector3.Distance(debugHardPoints[i][j], debugHardPoints[i - 1][j]);
-
-                    // Direction from previous to current position
-                    Vector3 direction = (debugHardPoints[i][j] - debugHardPoints[i - 1][j]).normalized;
-
-                    RaycastHit hit;
-                    if (Physics.Raycast(debugHardPoints[i - 1][j], direction, out hit, distance))
+                    // If weapon has hit the enemy make line red
+                    if (debugHardPoints[i][j].Item2)
                     {
-                        // If it is an enemy
-                        if (hit.collider.tag == "Enemy")
-                        {
-                            Gizmos.color = Color.yellow;
-                        }
+                        Gizmos.color = Color.red;
                     }
-
-                    Gizmos.DrawLine(debugHardPoints[i][j], debugHardPoints[i - 1][j]);
+                    else
+                    {
+                        Gizmos.color = Color.blue;
+                    }
+                    
+                    Gizmos.DrawLine(debugHardPoints[i][j].Item1, debugHardPoints[i - 1][j].Item1);
                 }
             }
         }
@@ -87,6 +72,8 @@ public class WeaponAttack : MonoBehaviour
     {
         if (attackDetected)
         {
+            #region Get HardPoints in weapon
+
             List<Vector3> hardPoints = new List<Vector3>();
 
             // Get children(hardPoint) position of the weapon
@@ -95,16 +82,26 @@ public class WeaponAttack : MonoBehaviour
                 hardPoints.Add(transform.GetChild(i).position);
             }
 
-            // If previous hardPoint does exist
+            #endregion
+
+            // Debug
+            List<(Vector3, bool)> storeHardPoints = new List<(Vector3, bool)>();
+
+            // If previous hardPoint does exist (Need previous hardpoint to raycast)
             if (preHardPoints.Count > 0)
             {
+                // Loops through hardpoints
                 for (int i = 0; i < hardPoints.Count; i++)
                 {
+                    #region Calculate Raycasting Weapon Detection
+
                     // Distance between current weapon position and last weapon position
                     float distance = Vector3.Distance(hardPoints[i], preHardPoints[i]);
 
                     // Direction from previous to current position
                     Vector3 direction = (hardPoints[i] - preHardPoints[i]).normalized;
+
+                    #region Raycast
 
                     RaycastHit hit;
                     if (Physics.Raycast(preHardPoints[i], direction, out hit, distance))
@@ -112,24 +109,41 @@ public class WeaponAttack : MonoBehaviour
                         // If it is an enemy
                         if (hit.collider.tag == "Enemy")
                         {
+                            #region Hit Enemy
+
                             // Show where in the swing the attack is detected
                             if (showDebugLines)
-                                hardPointHit = hardPoints;
+                                storeHardPoints.Add((hardPoints[i], true)); // Adding a tuple
 
                             // Enemy Take Damage
                             Debug.Log("hit enemy");
-                            hit.collider.gameObject.GetComponent<EnemyStats>().TakeDamage(attackDamage);                           
+                            hit.collider.gameObject.GetComponent<EnemyStats>().TakeDamage(attackDamage);
+
+                            #endregion
+                        }
+                        else
+                        {
+                            storeHardPoints.Add((hardPoints[i], false)); // Adding a tuple
                         }
                     }
+                    else
+                    {
+                        storeHardPoints.Add((hardPoints[i], false)); // Adding a tuple
+                    }
+
+                    #endregion
+
+                    #endregion
                 }
             }
 
+            // Show swing lines
+            if (preHardPoints.Count > 0 && showDebugLines)
+                debugHardPoints.Add(storeHardPoints);
+
+
             // Store current hardpoint to previous for next raycasting check
             preHardPoints = hardPoints;
-
-            // Show swing lines
-            if (showDebugLines)
-                debugHardPoints.Add(hardPoints);
         }
     }
 
@@ -141,7 +155,6 @@ public class WeaponAttack : MonoBehaviour
             if (showDebugLines)
             {
                 debugHardPoints.Clear();
-                hardPointHit = new List<Vector3>();
             }
 
             // Clear previous hardPoint
