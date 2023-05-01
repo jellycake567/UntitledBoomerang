e8c8671cad9fed3b433cde6a22ac662e387da1d0
Samuel Le <robbo_rules1@live.com.au>
2023-05-01 13:31:37 +1000

    Updated player stats

--- UntitledFoxSpirit/Assets/Scripts/Player/PlayerStats.cs
+++ UntitledFoxSpirit/Assets/Scripts/Player/PlayerStats.cs
@@ -1,17 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     [Header("Stats")]
     public float health = 100f;
+    private float currentHealth;
     public float invulnerableDelay = 1.0f;
     bool isInvulnerable = false;
 
+    [Header("Potion")]
+    [SerializeField] int numOfPotions = 3;
+    [SerializeField] float potionHeal = 50f;
+    private int currentPotions;
+    
     [Header("References")]
     public Material defaultStateMat;
     public Material hurtStateMat;
+    [SerializeField] Slider healthBar;
+    [SerializeField] TMP_Text potionNumDisplay;
+
+    void Start()
+    {
+        currentHealth = health;
+        healthBar.value = currentHealth / health;
+
+        currentPotions = numOfPotions;
+        potionNumDisplay.text = currentPotions.ToString();
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.N))
+        //{
+        //    TakeDamage(10f);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            UsePotion();
+        }
+    }
 
     public void TakeDamage(float damage)
     {
@@ -34,5 +66,21 @@ public class PlayerStats : MonoBehaviour
         
         isInvulnerable = false; //does not hit??????????
         //playerMesh.material = defaultStateMat;
+    }
+
+    void UsePotion()
+    {
+        if (currentPotions > 0)
+        {
+            currentHealth += potionHeal;
+
+            if (currentHealth > health)
+                currentHealth = health;
+
+            healthBar.value = currentHealth / health;
+
+            currentPotions--;
+            potionNumDisplay.text = currentPotions.ToString();
+        }
     }
 }
