642d1239985b0159f41db21f8ff9494c513f037a
Samuel Le <robbo_rules1@live.com.au>
2023-06-05 16:41:00 +1000

    Custom inspector done

--- UntitledFoxSpirit/Assets/Editor/PlayerEditor.cs
+++ UntitledFoxSpirit/Assets/Editor/PlayerEditor.cs
@@ -10,17 +10,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Analytics;
 using UnityEngine.WSA;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
 
 [CustomEditor(typeof(PlayerStateMachine))]
 public class PlayerEditor : Editor
 {
-    List<string> CategoryList = new List<string>{"MOVEMENT", "ROTATION", "GRAVITY",
-                                                 "JUMP", "SNEAKING", "DASH", "LEDGECLIMB", "ATTACK",
-                                                 "STAMINA", "TAKEDAMAGE", "CAMERA", "PATH" };
+    List<string> CategoryList = (new List<string>{"MOVEMENT", "ROTATION", "GRAVITY",
+                                                 "JUMP", "SNEAKING", "DASH", "LEDGE_CLIMB", "ATTACK",
+                                                 "STAMINA", "TAKE_DAMAGE", "CAMERA", "PATH", "OTHER" });
 
 
     PlayerStateMachine obj;
     SerializedObject vsoObject;
+    VariableScriptObject vso;
+
+    SerializedProperty property;
+    bool showPosition;
+    bool beginBox = false;
+    int countHeader;
 
     void OnEnable()
     {
@@ -29,7 +38,7 @@ public class PlayerEditor : Editor
 
         SerializedProperty playerData = serializedObject.FindProperty("vso");
         vsoObject = new SerializedObject(playerData.objectReferenceValue);
-
+        vso = (VariableScriptObject)playerData.objectReferenceValue;
     }
 
     public override void OnInspectorGUI()
@@ -38,27 +47,56 @@ public class PlayerEditor : Editor
 
         LoadScriptableObject();
 
+        // Style the header of foldout
+        GUIStyle style = EditorStyles.foldout;
+        style.fontSize = 13;
+        style.fontStyle = FontStyle.Bold;
 
+        // Get all variables
         FieldInfo[] fields = vsoObject.targetObject.GetType().GetFields();
 
         foreach (FieldInfo field in fields)
         {
+            // Check if variable is a header
             if (CategoryList.Contains(field.Name))
             {
+                if (!beginBox)
+                {
+                    beginBox = true;
+                }
+                else
+                {
+                    EditorGUILayout.EndVertical();
+                }
 
+                EditorGUILayout.Space();
+                EditorGUILayout.BeginVertical("Box");
 
+                string name = field.Name;
+                name = name.Replace("_", " ");
 
+                // Get bool value from scriptable object
+                showPosition = (bool)field.GetValue(vsoObject.targetObject);
+
+                // Create foldout and get bool if foldout is expanded
+                showPosition = EditorGUILayout.Foldout(showPosition, name, true, style);
+
+                // Set bool value in scriptable object
+                field.SetValue(vsoObject.targetObject, showPosition);
 
                 continue;
             }
 
+            if (showPosition)
+                EditorGUILayout.PropertyField(vsoObject.FindProperty(field.Name));
 
-            EditorGUILayout.PropertyField(vsoObject.FindProperty(field.Name));
-
+            
         }
 
-        base.OnInspectorGUI();
-        
+
+        if (showPosition)
+            base.OnInspectorGUI();
+
         //Component[] comArr = obj.GetComponents<Component>();
         //foreach (Component com in comArr)
         //{
@@ -78,6 +116,8 @@ public class PlayerEditor : Editor
 
         vsoObject.ApplyModifiedProperties();
         serializedObject.ApplyModifiedProperties();
+
+        
     }
 
     void LoadScriptableObject()
--- UntitledFoxSpirit/Assets/Scripts/Player/StateMachine/VariableScriptObject.cs
+++ UntitledFoxSpirit/Assets/Scripts/Player/StateMachine/VariableScriptObject.cs
@@ -10,9 +10,8 @@ public class VariableScriptObject : ScriptableObject
 {
     #region Movement / Rotation
 
-    public static bool MOVEMENT; // Never use, editor stuff
+    public bool MOVEMENT = false; // Never use, editor stuff
 
-    [Header("Movement")]
     public float humanSpeed = 5.0f;
     public float humanRunSpeed = 10.0f;
     public float accelTimeToMaxSpeed = 2.0f;
@@ -27,15 +26,13 @@ public class VariableScriptObject : ScriptableObject
     public float frictionAmount = 0.2f;
 
 
-    public static bool ROTATION; // Never use, editor stuff
+    public bool ROTATION = false; // Never use, editor stuff
 
-    [Header("Rotation")]
     public float timeToReachTargetRotation = 0.14f;
 
 
-    public static bool GRAVITY; // Never use, editor stuff
+    public bool GRAVITY = false; // Never use, editor stuff
 
-    [Header("Gravity")]
     public float gravity = -9.81f;
     public float gravityScale = 3f;
     public float fallGravityMultiplier = 0.2f;
@@ -49,9 +46,8 @@ public class VariableScriptObject : ScriptableObject
 
     #region Player Actions
 
-    public static bool JUMP; // Never use, editor stuff
+    public bool JUMP = false; // Never use, editor stuff
 
-    [Header("Jump")]
     public float humanJumpHeight = 5f;
     public float jumpRollVelocity = -5f;
     public float rootMotionJumpRollSpeed = 2f;
@@ -62,25 +58,22 @@ public class VariableScriptObject : ScriptableObject
     public float doubleJumpHeightPercent = 0.5f;
 
 
-    public static bool SNEAKING; // Never use, editor stuff
+    public bool SNEAKING = false; // Never use, editor stuff
 
-    [Header("Sneaking")]
     public float sneakSpeed = 2f;
     public Vector3 sneakCheckOffset;
     public Vector3 sneakCheckSize;
 
 
-    public static bool DASH; // Never use, editor stuff
+    public bool DASH = false; // Never use, editor stuff
 
-    [Header("Dash")]
     public float humanDashTime = 5.0f;
     public float humanDashDistance = 4.0f;
     public float dashCooldown = 1f;
 
 
-    public static bool LEDGECLIMB; // Never use, editor stuff
+    public bool LEDGE_CLIMB = false; // Never use, editor stuff
 
-    [Header("Ledge Climb")]
     public float wallCheckDistance = 3.0f;
     public float ledgeHangDistanceOffset;
     public float ledgeHangYOffset;
@@ -89,9 +82,8 @@ public class VariableScriptObject : ScriptableObject
     public LayerMask groundLayer;
 
 
-    public static bool ATTACK; // Never use, editor stuff
+    public bool ATTACK = false; // Never use, editor stuff
 
-    [Header("Attack")]
     public float attackCooldown = 2f;
     public float resetComboDelay = 1f;
     public float rootMotionAtkSpeed = 2f;
@@ -101,37 +93,36 @@ public class VariableScriptObject : ScriptableObject
 
     #region Other
 
-    public static bool STAMINA; // Never use, editor stuff
+    public bool STAMINA = false; // Never use, editor stuff
 
-    [Header("Stamina")]
     public float staminaConsumption = 20f;
     public float staminaRecovery = 5f;
     public float staminaCooldown = 1f;
     public float maxStamina = 100f;
 
 
-    public static bool TAKEDAMAGE; // Never use, editor stuff
+    public bool TAKE_DAMAGE = false; // Never use, editor stuff
 
-    [Header("Take Damage")]
     public float invulnerableTime = 1f;
     public float regainMovement = 0.5f;
     public float horizontalKnockback = 10f;
     public float verticalKnockback = 10f;
 
 
-    public static bool CAMERA; // Never use, editor stuff
+    public bool CAMERA = false; // Never use, editor stuff
 
-    [Header("Camera")]
     public float camRotationSpeed2D = 0.2f;
 
 
-    public static bool PATH; // Never use, editor stuff
+    public bool PATH = false; // Never use, editor stuff
 
-    [Header("Path")]
     public float maxDistancePath = 0.5f;
     public float distanceSpawn = 0f;
     public float spawnYOffset = 0f;
     public float adjustVelocity = 1.0f; //Velocity to push player towards the path
+
+
+    public bool OTHER = false; // Never use, editor stuff
 
     #endregion                                                                                                                                                                                                                                                                
 }
